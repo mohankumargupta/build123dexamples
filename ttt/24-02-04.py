79afdfdf0cf14d3c3d3c61a55de2cb4fd89a7707
Raspberry Pi <raspberrypisig@gmail.com>
2024-12-22 14:13:24 +1100

    slotcentertocenter

--- ttt/24-02-04.py
+++ ttt/24-02-04.py
@@ -15,9 +15,8 @@ with BuildPart() as part:
     # base part
     with BuildSketch() as bottom_sketch:
         RectangleRounded(LENGTH, WIDTH, radius=10)
-        SLOT_RADIUS = 15
-        LEFT_SLOT_CENTER = HALF_LENGTH - (LENGTH - 108)/2
-        s1 = SlotOverall(50, 12, mode=Mode.PRIVATE)
+        CENTER_TO_CENTER_DISTANCE = LENGTH - 108
+        s1 = SlotCenterToCenter(center_separation=CENTER_TO_CENTER_DISTANCE, height=12, mode=Mode.PRIVATE)
         s2 = split(s1, bisect_by=Plane.YZ, mode=Mode.PRIVATE, keep=Keep.BOTTOM)
         with Locations((HALF_LENGTH,0)):
             s3 = add(s2, mode=Mode.SUBTRACT)
