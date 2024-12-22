5aee4d4da353d5557454d20305ff22810cbadc03
Raspberry Pi <raspberrypisig@gmail.com>
2024-12-22 13:47:37 +1100

    base part of ttt 24-02-4

--- ttt/24-02-04.py
+++ ttt/24-02-04.py
@@ -1,14 +1,26 @@
 from build123d import *
-from ocp_vscode import show_all, Camera
+from ocp_vscode import show, show_all, Camera
 
-length = 165
-width = 85
-height = 12
+# Parameters
+
+LENGTH = 165
+WIDTH = 85
+HEIGHT = 12
+
+# Calculated values from parameters
+HALF_LENGTH = LENGTH / 2
 
 with BuildPart() as part:
+    # base part
     with BuildSketch() as bottom_sketch:
-        RectangleRounded(length, width, radius=10)
-    extrude(amount=-height)
+        RectangleRounded(LENGTH, WIDTH, radius=10)
+        SLOT_RADIUS = 15
+        LEFT_SLOT_CENTER = HALF_LENGTH - (LENGTH - 108)/2
+        s1 = SlotOverall(50, 12, mode=Mode.PRIVATE)
+        s2 = split(s1, bisect_by=Plane.YZ, mode=Mode.PRIVATE, keep=Keep.BOTTOM)
+        with Locations((HALF_LENGTH,0)):
+            s3 = add(s2, mode=Mode.SUBTRACT)
+    extrude(amount=-HEIGHT)
 
 show_all(axes=True, axes0=True, transparent=True)
 #show(part, reset_camera=Camera.KEEP)
