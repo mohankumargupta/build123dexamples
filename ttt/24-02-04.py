11206969be7bdcc51b8b3a5ab693666b9a1264cc
Raspberry Pi <raspberrypisig@gmail.com>
2024-12-23 11:19:08 +1100

    progress

--- ttt/24-02-04.py
+++ ttt/24-02-04.py
@@ -4,25 +4,33 @@ from ocp_vscode import show, show_all, Camera
 # Parameters
 LENGTH = 165
 WIDTH = 85
-HEIGHT = 12
+HEIGHT = 25
+BASE_HEIGHT = 12
 SLOT_INNER_WIDTH = 15
+LIP_OFFSET = 5
+RAMP_LENGTH = 90
+RAMP_WIDTH = 65
+RAMP_HEIGHT = HEIGHT - BASE_HEIGHT
 
 # Calculated values from parameters
 HALF_LENGTH = LENGTH / 2
 HALF_WIDTH = WIDTH / 2
+HALF_RAMP_LENGTH = RAMP_LENGTH / 2
+HALF_RAMP_WIDTH = RAMP_WIDTH / 2
+HALF_RAMP_HEIGHT = RAMP_HEIGHT / 2
 
 with BuildPart() as part:
     # base body
     with BuildSketch() as base_sketch:
         RectangleRounded(LENGTH, WIDTH, radius=10)
-    extrude(amount=-HEIGHT)
+    extrude(amount=-BASE_HEIGHT)
     
     # lip extrude
     with BuildSketch(mode=Mode.PRIVATE) as half_slot_sketch:
         CENTER_TO_CENTER_DISTANCE = LENGTH - 108
-        LIP_OFFSET = 5
+        
         s1 = SlotCenterToCenter(center_separation=CENTER_TO_CENTER_DISTANCE, height=SLOT_INNER_WIDTH+2*LIP_OFFSET)
-        s2 = offset(amount=-5, mode=Mode.SUBTRACT)
+        s2 = offset(amount=-LIP_OFFSET, mode=Mode.SUBTRACT)
         s3 = split(bisect_by=Plane.YZ, keep=Keep.BOTTOM)
     with BuildSketch() as lip_sketch:
         with Locations((HALF_LENGTH, 0)):
@@ -35,13 +43,26 @@ with BuildPart() as part:
         s5 = split(s4, bisect_by=Plane.YZ, keep=Keep.BOTTOM, mode=Mode.PRIVATE)
         with Locations((HALF_LENGTH, 0)):
             add(s5)
-    extrude(amount=-HEIGHT, mode=Mode.SUBTRACT)
+    extrude(amount=-BASE_HEIGHT, mode=Mode.SUBTRACT)
     
     #mirror lip extrude and slot hole
     split(bisect_by=Plane.YZ, keep=Keep.TOP)
     mirror(about=Plane.YZ)
-    
+
+with BuildPart() as part2:
+    with BuildSketch(Plane.XZ) as ramp_sketch:
+        RAMP_RADIUS = 50
+        ARC_OFFSET = 5
+        with BuildLine() as ramp_profile:
+            ramp_l1 = Line((-HALF_RAMP_WIDTH, -HALF_RAMP_HEIGHT), (HALF_RAMP_WIDTH, -HALF_RAMP_HEIGHT))
+            ramp_l2 = Line(ramp_l1@1, ramp_l1@1 + (0, RAMP_HEIGHT))
+            ramp_l3 = Line(ramp_l2@1, ramp_l2@1 + (-ARC_OFFSET, 0))
+            ramp_l4 = Line(ramp_l1@0, ramp_l1@0 + (0, RAMP_HEIGHT))
+            ramp_l5 = Line(ramp_l4@1, ramp_l4@1 + (ARC_OFFSET, 0))
+            ramp_l6 = RadiusArc(ramp_l5@1, ramp_l3@1, radius=-RAMP_RADIUS)
+        make_face()
+    extrude(amount=HALF_RAMP_LENGTH, both=True)
        
 #show_all(axes=True, axes0=True, transparent=True)
-show_all(axes=True, axes0=True, transparent=False)
+show_all(axes=True, axes0=True, transparent=False, reset_camera=Camera.KEEP)
         
